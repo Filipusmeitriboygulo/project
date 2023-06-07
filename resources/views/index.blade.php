c51c4bcf835e1c64a3214b7ba7f268162dd527ad
Filipusmeitriboygulo <filipusmeitriboygulo@gmail.com>
2023-06-07 13:54:58 +0700

    commit pertama

---
+++ resources/views/index.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=" 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    {{-- Font   --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@700;900&family=Lato:ital,wght@1,700;1,900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/style.css') }}">
+
+    <title>CS_Outlet88s</title>
+    
+</head>
+<body>
+  <header>
+    {{-- navbar --}}
+    <div class="row">
+      <div class="col">
+        <nav class="navbar navbar-expand-lg ">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">CS_Outlet88</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active"  h ref="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active"  href="#">Promo</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active"  href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active"  href="#">About Us</a>
+                </li>
+                
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              
+              
+              </form>
+              <div class="icons-home">
+                  
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+    
+
+  </header>
+      {{-- Container --}}
+
+      <div class="container">
+        <!-- Content here -->
+        <div class="row">
+          <div class="col">
+            <h1>Temukan Fashion Yang Paling Cocok Denganmu</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum earum adipisci maxime harum beatae dicta. Ea repellat ipsam magnam hic.</h3>
+          </div>
+          <div class="col">
+            
+          </div>
+        </div>
+        
+      </div>
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
